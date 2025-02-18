bfbaed5c0a73ba01e53890aa824de92822cafd09
Pulak5376 <nath15-5376@diu.edu.bd>
2025-02-18 10:37:04 +0600

    Task_01

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Labclass5());
+}
+
+class Labclass5 extends StatelessWidget {
+  const Labclass5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Text("Basic Flutter UI- 02"),
+      backgroundcolor: Colors.blue(400),
+      centerTitle: true,
+    );
+    
+
+  }
 }
