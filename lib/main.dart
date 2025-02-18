335805458167bad17201f9e629ed6b23d55f9d3c
Pulak5376 <nath15-5376@diu.edu.bd>
2025-02-18 10:38:18 +0600

    task_01

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Labclass5());
+  runApp(const LabClass05());
 }
 
-class Labclass5 extends StatelessWidget {
-  const Labclass5({super.key});
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: Text("Basic Flutter UI- 02"),
-      backgroundcolor: Colors.blue(400),
-      centerTitle: true,
-    );
-    
-
+        title: 'Basic Flutter UI 02',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Basic Flutter"),
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+          ),
+          body: Row(
+            children: [
+              Icon(Icons.star,
+                  size: 50, color: const Color.fromARGB(210, 129, 34, 180)),
+              Icon(Icons.star,
+                  size: 50, color: const Color.fromARGB(210, 135, 13, 98)),
+              Icon(Icons.star,
+                  size: 50, color: const Color.fromARGB(210, 57, 42, 52)),
+              Icon(Icons.star,
+                  size: 50, color: const Color.fromARGB(210, 172, 47, 134)),
+              Icon(Icons.star,
+                  size: 50, color: const Color.fromARGB(41, 185, 33, 140)),
+            ],
+          ),
+        ));
   }
 }
